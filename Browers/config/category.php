24ba60653df460a4b62dd06542f28bc000cbeb34
kiena123 <90166455+kiena123@users.noreply.github.com>
2021-10-26 21:44:29 +0700

    them

---
+++ Browers/config/category.php
@@ -0,0 +1,19 @@
+                    <div id="category" class="col col-md-3 bg-dark text-light position-fixed overflow-auto" style="left: -700px; height: 670px">
+                        <div class="text-light my-2">
+                            <a href="" class="text-decoration-none text-danger">Trang chủ</a>
+                        </div>
+                        <div class="text-light my-2">
+                            Lịch
+                            <ul class="">
+
+                            </ul>
+                        </div>
+                        <div class="text-light my-2">
+                            Các kế hoạch
+                            <ul></ul>
+                        </div>
+                        <div class="text-light my-2">
+                            Nhóm của bạn
+                            <ul></ul>
+                        </div>
+                    </div>
